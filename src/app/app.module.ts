c9e01572adf71995508937300041518af05ced30
Arijit Gupta <arijitgupta2000@gmail.com>
2019-07-11 22:14:59 +0530

    Few Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClient } from '@angular/common/http'
 
 
 import { CustomMaterialModule } from './material.module'
